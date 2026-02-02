7a14236c7360f76a9bb1426eb2e7f16631f4ca67
sgizemmazi <sgizemmazi@gmail.com>
2026-02-02 11:28:07 +0300

    fix: edit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import MobileLayoutWarm from '@/components/layout/MobileLayoutWarm';
-import HeaderWarm from '@/components/home/HeaderWarm';
-import QuickActionsWarm from '@/components/home/QuickActionsWarm';
-import DialControlWarm from '@/components/home/DialControlWarm';
-import DeviceCardsWarm from '@/components/home/DeviceCardsWarm';
-import { LevelUpCelebration } from '@/components/gamification/LevelUpCelebration';
-import { usePersistedGameProgress } from '@/hooks/usePersistedGameProgress';
-import { motion } from 'framer-motion';
+import React from "react";
+import MobileLayoutWarm from "@/components/layout/MobileLayoutWarm";
+import HeaderWarm from "@/components/home/HeaderWarm";
+import QuickActionsWarm from "@/components/home/QuickActionsWarm";
+import DialControlWarm from "@/components/home/DialControlWarm";
+import DeviceCardsWarm from "@/components/home/DeviceCardsWarm";
+import { LevelUpCelebration } from "@/components/gamification/LevelUpCelebration";
+import { usePersistedGameProgress } from "@/hooks/usePersistedGameProgress";
+import { motion } from "framer-motion";
 
 const Index: React.FC = () => {
-  const { progress, levelUpData, closeLevelUpCelebration } = usePersistedGameProgress();
+  const { progress, levelUpData, closeLevelUpCelebration } =
+    usePersistedGameProgress();
 
   return (
     <MobileLayoutWarm>
-      <motion.div 
+      <motion.div
         className="space-y-0"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -21,16 +22,16 @@ const Index: React.FC = () => {
       >
         {/* Header with status info */}
         <HeaderWarm />
-        
-        {/* Quick Actions Grid - Rooms style */}
-        <QuickActionsWarm />
-        
+
         {/* Dial Control - Temperature style */}
         <DialControlWarm progress={progress} />
-        
+
+        {/* Quick Actions Grid - Rooms style */}
+        <QuickActionsWarm />
+
         {/* Device Cards - Bottom devices */}
-        <DeviceCardsWarm />
-        
+        {/* <DeviceCardsWarm /> */}
+
         {/* Bottom spacing */}
         <div className="h-4" />
       </motion.div>
